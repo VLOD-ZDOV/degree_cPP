5b8d4a8e5442b95b3b9386a11c14b41663f2d970
Vlodimir <66350035+VLOD-ZDOV@users.noreply.github.com>
2024-03-09 12:47:15 +0700

    Код для нахождения степеней по порядку...

---
+++ Степени.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    using namespace std;
+    while (true) {
+
+
+        cout << "Введи число:" << endl;
+        unsigned long long numbers;
+        cin >> numbers;
+
+        cout << "введи степень" << endl;
+        unsigned long long degree;
+        cin >> degree;
+
+        if (numbers and degree == 0) {
+            cout << "Неопределенность" << endl;
+        }
+        else if (numbers == 1) {
+            cout << "Будет всегда 1!" << endl;
+        }
+        else if (degree == 0) {
+            cout << "Будет всегда 1" << endl;
+        }
+        //else if (degree < 0) {
+        //    long double a = 1 / numbers;           В РАЗРАБОТКЕ!!!!!!!
+        //    long double result = a 
+        //}
+        else {
+            ;
+            for (int i = 1; i <= degree; i++) {
+                unsigned long long result = pow(numbers, i);
+                std::cout << result << " это " << i << " степень" << std::endl;
+            }
+        }
+
+    }
+    }
